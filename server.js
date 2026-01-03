6a9de02409788faee6d56e02a22bef06837390a2
sohaibsaadat <sohaibsaadat9@gmail.com>
2026-01-03 22:34:04 +0500

    condition added for server

--- server.js
+++ server.js
@@ -45,7 +45,12 @@ app.use("/api/messages", messageRouter)
 
 await connectDB()
 
-const PORT = process.env.PORT || 5000
+if(process.env.NODE_ENV === 'development'){
+  const PORT = process.env.PORT || 5000
 server.listen(PORT, () =>
   console.log("Server running on port:", PORT)
 )
+
+}
+export default server
+
